e0d1306a4f873ee545189080b78e3a62fcb24b13
Maria <maria@maria-laptop.(none)>
2013-12-29 19:01:51 +0100

    added local settings file

---
+++ website/website/local_settings_example.py
@@ -0,0 +1,19 @@
+#LOCAL_SETTINGS = True
+#from settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = 'rlkw#_emt=s1ndhpsl$*l8duckx@784zde15&&v!^ta)xx6oni'
+
+#DEBUG = True #True | False
+#TEMPLATE_DEBUG = True
+#ALLOWED_HOSTS = []
+
+#STATICFILES_FINDERS = (
+	#'django.contrib.staticfiles.finders.FileSystemFinder',
+	#'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+	#'django.contrib.staticfiles.finders.DefaultStorageFinder',
+#)
+
+#INSTALLED_APPS = (
+	#'django.contrib.staticfiles',
+#)
